4454beab7a5bdae0a973c35feb999e03db918950
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-07 13:15:07 -0400

    Add post and delete fetch logic

--- src/App/App.js
+++ src/App/App.js
@@ -24,15 +24,37 @@ class App extends Component {
     fetch('http://localhost:3001/api/v1/reservations')
     .then(res => this.checkForError(res))
     .then(data => this.setState({reservations: data}))
+    .catch(err => alert(err.message))
   }
 
   createRes = (resObj) => {
     if (!isNaN(resObj.number)) {
-      console.log('hello')
       this.setState({reservations: [...this.state.reservations, resObj]})
+      this.postRes(resObj);
     } else {
       this.setState({...this.state, error: 'Need a number for guests'})
     }
+  }
+
+  postRes = (obj) => {
+    fetch('http://localhost:3001/api/v1/reservations', {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify(obj)
+    })
+    .then(res => this.checkForError(res))
+    .catch(err => alert(err.message))
+  }
+
+  cancelRes = (id) => {
+    const reservations = this.state.reservations
+    const resIndex = reservations.findIndex(res => res.id === id)
+    reservations.splice(resIndex, 1)
+    this.setState({...this.state, reservations: reservations})
+    fetch('http://localhost:3001/api/v1/reservations/' + id, {
+      method: 'DELETE',
+      headers: {'Content-type': 'application/json'}
+    })
   }
 
   render() {
@@ -41,7 +63,7 @@ class App extends Component {
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         {this.state.error && <p className="error">{this.state.error}</p>}
         <Form createRes={this.createRes}></Form>
-        <Reservations resys={this.state.reservations}>
+        <Reservations resys={this.state.reservations} cancelRes={this.cancelRes}>
         </Reservations>
       </div>
     )
