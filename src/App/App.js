6bc16d9e8919c35b3d9f5f46be022e579f49aa6f
Jackson McGuire <jaxmcguire@gmail.com>
2021-04-07 11:39:14 -0400

    Create initial fetch and render resys

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Reservations } from '../Reservations/Reservations'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  checkForError = (response) => {
+    if (!response.ok) {
+      throw new Error(response.message);
+    } else {
+      return response.json();
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(res => this.checkForError(res))
+    .then(data => this.setState({reservations: data}))
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,9 +31,8 @@ class App extends Component {
         <div className='resy-form'>
 
         </div>
-        <div className='resy-container'>
-          
-        </div>
+        <Reservations resys={this.state.reservations} className='resy-container'>
+        </Reservations>
       </div>
     )
   }
